48ffe4e30b3eed38461033512f0a5d895b38866a
Jun Hyuk Han <hanjun.1998@gmail.com>
2020-07-21 23:59:25 +0900

    .

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,48 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit'
+
+const initialState = [
+    { id: '1', date: '2011-10-05T14:48:00.000Z', title: 'First Post!', content: 'Hello!' , user: "0", },
+    { id: '2', date: '2012-10-05T14:48:00.000Z', title: 'Second Post', content: 'More text' }
+]
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {
+        postAdded: {
+            reducer(state, action) {
+                state.push(action.payload)
+            },
+            prepare(title, content, userId) {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        date: new Date().toISOString(),
+                        title,
+                        content,
+                        user: userId
+                    }
+                }
+            }
+        },
+        postUpdated(state, action) {
+            const { id, title, content } = action.payload
+            const existingPost = state.find(post => post.id === id)
+            if (existingPost) {
+                existingPost.title = title
+                existingPost.content = content
+            }
+        },
+        reactionAdded(state, action) {
+            const { postId, reaction } = action.payload 
+            const existingPost = state.find(post => post.id === postId)
+            if (existingPost) {
+                existingPost.reactions[reaction]++
+            }
+        }
+    }
+})
+
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
+
+export default postsSlice.reducer
